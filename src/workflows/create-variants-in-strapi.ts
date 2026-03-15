ac866bf945ff6064bb24302f7e038cdcf2433c7f
Omar-Alkhateeb <omar.alkhateeb9919@gmail.com>
2026-03-16 01:15:44 +0300

    first commit

--- src/workflows/create-variants-in-strapi.ts
+++ src/workflows/create-variants-in-strapi.ts
@@ -50,100 +50,92 @@ export const createVariantsInStrapiWorkflow = createWorkflow(
       },
     });
 
-    const strapiVariants = when(
-      { variants },
-      // 🟢 THE FIX: Check the metadata for the Strapi ID instead
-      (data) => !!data.variants[0].product?.metadata?.strapi_id,
-    ).then(() => {
-      const variantImages = transform(
-        {
-          variants,
-        },
-        (data) => {
-          return data.variants.flatMap(
-            (variant) =>
-              variant.images?.map((image) => ({
-                entity_id: variant.id,
-                url: image.url,
-              })) || [],
-          );
-        },
-      );
-      const variantThumbnail = transform(
-        {
-          variants,
-        },
-        (data) => {
-          return (
-            data.variants
+    const variantImages = transform(
+      {
+        variants,
+      },
+      (data) => {
+        return data.variants.flatMap(
+          (variant) =>
+            variant.images?.map((image) => ({
+              entity_id: variant.id,
+              url: image.url,
+            })) || [],
+        );
+      },
+    );
+    const variantThumbnail = transform(
+      {
+        variants,
+      },
+      (data) => {
+        return (
+          data.variants
+            // @ts-ignore
+            .filter((variant) => !!variant.thumbnail)
+            .flatMap((variant) => ({
+              entity_id: variant.id,
               // @ts-ignore
-              .filter((variant) => !!variant.thumbnail)
-              .flatMap((variant) => ({
-                entity_id: variant.id,
-                // @ts-ignore
-                url: variant.thumbnail!,
-              }))
-          );
-        },
-      );
+              url: variant.thumbnail!,
+            }))
+        );
+      },
+    );
 
-      const strapiVariantImages = uploadImagesToStrapiStep({
-        items: variantImages,
-      });
-
-      const strapiVariantThumbnail = uploadImagesToStrapiStep({
-        items: variantThumbnail,
-      }).config({ name: "upload-variant-thumbnail" });
-
-      const variantsData = transform(
-        { variants, strapiVariantImages, strapiVariantThumbnail },
-        (data) => {
-          const varData = data.variants.map((variant) => ({
-            id: variant.id,
-            title: variant.title,
-            sku: variant.sku,
-            strapiProductId: Number(variant.product?.metadata?.strapi_id),
-            strapiVariantImages: data.strapiVariantImages
-              .filter((image) => image.entity_id === variant.id)
-              .map((image) => image.image_id),
-            strapiVariantThumbnail: data.strapiVariantThumbnail.find(
-              (image) => image.entity_id === variant.id,
-            )?.image_id,
-            optionValueIds: variant.options.flatMap((option) => {
-              // find the strapi option value id for the option value
-              return variant.product?.options
-                .flatMap(
-                  (productOption) =>
-                    productOption.values.find(
-                      (value) => value.value === option.value,
-                    )?.metadata?.strapi_id,
-                )
-                .filter((value) => value !== undefined);
-            }),
-          }));
-
-          return varData;
-        },
-      );
-
-      const strapiVariants = createVariantsInStrapiStep({
-        variants: variantsData,
-      } as CreateVariantsInStrapiInput);
-
-      const variantsMetadataUpdate = transform({ strapiVariants }, (data) => {
-        return {
-          updates: data.strapiVariants.map((strapiVariant) => ({
-            variantId: strapiVariant.medusaId,
-            strapiId: strapiVariant.id,
-            strapiDocumentId: strapiVariant.documentId,
-          })),
-        };
-      });
-
-      updateProductVariantsMetadataStep(variantsMetadataUpdate);
-
-      return strapiVariants;
+    const strapiVariantImages = uploadImagesToStrapiStep({
+      items: variantImages,
     });
+
+    const strapiVariantThumbnail = uploadImagesToStrapiStep({
+      items: variantThumbnail,
+    }).config({ name: "upload-variant-thumbnail" });
+
+    const variantsData = transform(
+      { variants, strapiVariantImages, strapiVariantThumbnail },
+      (data) => {
+        const varData = data.variants.map((variant) => ({
+          id: variant.id,
+          title: variant.title,
+          sku: variant.sku,
+          strapiProductId: Number(variant.product?.metadata?.strapi_id),
+          strapiVariantImages: data.strapiVariantImages
+            .filter((image) => image.entity_id === variant.id)
+            .map((image) => image.image_id),
+          strapiVariantThumbnail: data.strapiVariantThumbnail.find(
+            (image) => image.entity_id === variant.id,
+          )?.image_id,
+          optionValueIds: variant.options.flatMap((option) => {
+            // find the strapi option value id for the option value
+            return variant.product?.options
+              .flatMap(
+                (productOption) =>
+                  productOption.values.find(
+                    (value) => value.value === option.value,
+                  )?.metadata?.strapi_id,
+              )
+              .filter((value) => value !== undefined);
+          }),
+        }));
+
+        return varData;
+      },
+    );
+
+    const strapiVariants = createVariantsInStrapiStep({
+      variants: variantsData,
+    } as CreateVariantsInStrapiInput);
+
+    const variantsMetadataUpdate = transform({ strapiVariants }, (data) => {
+      return {
+        updates: data.strapiVariants.map((strapiVariant) => ({
+          variantId: strapiVariant.medusaId,
+          strapiId: strapiVariant.id,
+          strapiDocumentId: strapiVariant.documentId,
+        })),
+      };
+    });
+
+    updateProductVariantsMetadataStep(variantsMetadataUpdate);
 
     releaseLockStep({
       key: ["strapi-product-create", input.productId],
